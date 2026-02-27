6b8c9cc5dc16cd8ad873d6da52fc20946a2e742e
Alexander Zelayandia <alexzelayandia30@gmail.com>
2026-02-27 13:22:41 -0500

    Add second variable and prints average of the two

--- hello.c
+++ hello.c
@@ -4,5 +4,9 @@ int main() {
     printf("Hello Git Version 2!\n");
     int x = 0;
     printf("x = %d\n", x);
+
+    int y = 30;
+    float avg = (float) (x + y) / 2;
+    printf("average = %.2f\n", avg);
     return 0;
 }
